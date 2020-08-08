c8229e12361f9d6a41500ae7629e5c753de204a1
LimeZky <zoukeyi163@outlook.com>
2020-08-08 23:04:00 +0800

    Index

---
+++ asset/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?dd0d74a661842669ff212d0f39550c58";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
